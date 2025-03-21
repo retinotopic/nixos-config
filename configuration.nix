7164a658e36ae55b183991dea639c549a2b8abaf
retinotopic <retinotopic@proton.me>
2025-03-21 04:21:58 +0300

    waydroid + automount for thunar

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,8 @@
   services = {
     xserver.videoDrivers = ["nvidia"];
     fstrim.enable = true;
+    gvfs.enable = true;
+    tumbler.enable = true;
     pipewire = {
       enable = true;
       alsa.enable = true;
@@ -157,6 +159,7 @@
       enable = true;
       dockerCompat = true;
     };
+    waydroid.enable = true;
   };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
