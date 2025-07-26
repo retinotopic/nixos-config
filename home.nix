2f71967223d0c0d3192d4ee9b2d1645c7e680323
retinotopic <retinotopic@proton.me>
2025-07-26 17:38:49 +0300

    palette consistency4

--- home.nix
+++ home.nix
@@ -112,7 +112,7 @@
       ];
 
       interactiveShellInit = ''
-        set -g theme_nerd_fonts yes
+        set -g theme_nerd_fonts no
         set -g fish_prompt_prefix " "
     
         set -g theme_display_user yes
@@ -130,78 +130,43 @@
           set -l aqua 83c092
           set -l orange e69875
 
-          set -x color_initial_segment_exit     $dark $blue --bold
-          set -x color_initial_segment_private  $dark $red
-          set -x color_initial_segment_su       $dark $purple --bold
-          set -x color_initial_segment_jobs     $dark $yellow --bold
+          set -x color_initial_segment_exit     normal $blue --bold
+          set -x color_initial_segment_private  normal $red
+          set -x color_initial_segment_su       normal $purple --bold
+          set -x color_initial_segment_jobs     normal $yellow --bold
 
-          set -x color_path                     $dark $blue
-          set -x color_path_basename            $dark $red --bold
-          set -x color_path_nowrite             $dark $yellow
-          set -x color_path_nowrite_basename    $dark $purple --bold
+          set -x color_path                     normal $blue
+          set -x color_path_basename            normal $red --bold
+          set -x color_path_nowrite             normal $yellow
+          set -x color_path_nowrite_basename    normal $purple --bold
 
-          set -x color_repo                     $dark $blue
-          set -x color_repo_work_tree           $dark $red --bold
-          set -x color_repo_dirty               $dark $yellow
-          set -x color_repo_staged              $dark $purple
+          set -x color_repo                     normal $blue
+          set -x color_repo_work_tree           normal $red --bold
+          set -x color_repo_dirty               normal $yellow
+          set -x color_repo_staged              normal $purple
 
-          set -x color_vi_mode_default          $dark $blue --bold
-          set -x color_vi_mode_insert           $dark $red --bold
-          set -x color_vi_mode_visual           $dark $yellow --bold
+          set -x color_vi_mode_default          normal $blue --bold
+          set -x color_vi_mode_insert           normal $red --bold
+          set -x color_vi_mode_visual           normal $yellow --bold
 
-          set -x color_vagrant                  $dark $purple --bold
-          set -x color_aws_vault                $dark $yellow --bold
-          set -x color_aws_vault_expired        $dark $red --bold
-          set -x color_username                 $dark $blue --bold
+          set -x color_vagrant                  normal $purple --bold
+          set -x color_aws_vault                normal $yellow --bold
+          set -x color_aws_vault_expired        normal $red --bold
+          set -x color_username                 normal $blue --bold
 
-          set -x color_hostname                 $dark $blue
-          set -x color_rvm                      $dark $red --bold
-          set -x color_virtualfish              $dark $yellow --bold
-          set -x color_virtualgo                $dark $blue --bold
-          set -x color_desk                     $dark $aqua --bold
-          set -x color_nix                      $dark $purple --bold
+          set -x color_hostname                 normal $blue
+          set -x color_rvm                      normal $red --bold
+          set -x color_virtualfish              normal $yellow --bold
+          set -x color_virtualgo                normal $blue --bold
+          set -x color_desk                     normal $aqua --bold
+          set -x color_nix                      normal $purple --bold
 
         end
     
-        set -g theme_powerline_fonts yes
+        set -g theme_powerline_fonts no
         set -g fish_prompt_pwd_dir_length 3
       '';
       };
   };
 
 }
-#   plugins = [
-#     { name = "bobthefish"; src = pkgs.fishPlugins.bobthefish; }
-#   ];
-
-#   interactiveShellInit = ''
-#     set -g theme_nerd_fonts yes
-#     set -g fish_prompt_prefix " "
-    
-#     set -g theme_display_user yes
-#     set -g theme_display_hostname yes
-#     set -g theme_display_git_master_branch yes
-#     set -g theme_show_exit_status yes
-#     set -g theme_color_scheme user
-
-#     set -g theme_color_path              7fbbb3 #blue
-#     set -g theme_color_git               83c092 #aqua
-#     set -g theme_color_cwd               e67e80 #red
-#     set -g theme_color_git_worktree      dbbc7f #yellow
-#     set -g theme_color_error             d699b6 #purple
-#     set -g theme_color_command_duration  a7c080 #green
-#     set -g theme_color_virtualenv        d699b6 #purple
-
-#     set -g theme_color_user              7fbbb3
-#     set -g theme_color_host              83c092
-#     set -g theme_color_separator         333c43 #dark
-#     set -g theme_color_git_dirty         e69875 #orange
-#     set -g theme_color_git_staged        d699b6
-#     set -g theme_color_git_branch        a7c080
-#     set -g theme_color_sudo              e67e80
-#     set -g theme_color_jobs              dbbc7f
-
-#     set -g theme_powerline_fonts yes
-#     set -g fish_prompt_pwd_dir_length 3
-#   '';
-# };
