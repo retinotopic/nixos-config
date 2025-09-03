1dc6c4600e99173e7a071821e4ec518001bb8cbe
retinotopic <retinotopic@proton.me>
2025-09-03 19:02:10 +0300

    laptop power management

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -14,6 +14,8 @@
       settings = {
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
+        CPU_HWP_DYN_BOOST_ON_AC=1;
+        CPU_HWP_DYN_BOOST_ON_BAT=0;
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
@@ -23,13 +25,18 @@
         STOP_CHARGE_THRESH_BAT0 = 80;
         RADEON_DPM_STATE_ON_AC = "performance";
         RADEON_DPM_STATE_ON_BAT = "battery";
-        RADEON_DPM_PERF_LEVEL_ON_AC = "high";
-        RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
-
+        # RADEON_DPM_PERF_LEVEL_ON_AC = "high";
+        # RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
+        PLATFORM_PROFILE_ON_AC="performance";
+        PLATFORM_PROFILE_ON_BAT="low-power";
+        WIFI_PWR_ON_AC="on";
+        WIFI_PWR_ON_BAT="on";
+        RUNTIME_PM_ON_AC="auto";
+        RUNTIME_PM_ON_BAT="auto";
       };
     };
   };
-  #sudo ryzenadj --tctl-temp=75 --power-saving --fast-limit=10000 --slow-limit=10000 --stapm-limit=10000
+  #sudo ryzenadj --tctl-temp=75 --power-saving --fast-limit=5000 --slow-limit=5000 --stapm-limit=5000
   environment.systemPackages = [
     pkgs.ryzenadj
     pkgs.brightnessctl
